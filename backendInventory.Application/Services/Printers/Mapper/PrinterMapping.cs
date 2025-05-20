b9744fcb215608c4473120f582b91452d2e2ed37
Jonatas Figueiroba da Rocha <70000433@citrosuco.com>
2025-05-20 16:58:08 -0300

    Add Floor in PrinterViewModel

--- backendInventory.Application/Services/Printers/Mapper/PrinterMapping.cs
+++ backendInventory.Application/Services/Printers/Mapper/PrinterMapping.cs
@@ -11,10 +11,11 @@ public class PrinterMapping : MappingProfile
     {
         CreateMap<PrinterDTO, Printer>();
         CreateMap<Printer, PrinterViewModel>()
-            .ForMember(dest => dest.ManufacturerName, opt => opt.MapFrom(src => src.Manufacturer != null ? src.Manufacturer.Name : null))
-            .ForMember(dest => dest.ModelName, opt => opt.MapFrom(src => src.Model != null ? src.Model.Name : null))
-            .ForMember(dest => dest.UnitName, opt => opt.MapFrom(src => src.Unit != null ? src.Unit.Name : null))
-            .ForMember(dest => dest.BuildingName, opt => opt.MapFrom(src => src.Building != null ? src.Building.Name : null))
-            .ForMember(dest => dest.DepartmentName, opt => opt.MapFrom(src => src.Department != null ? src.Department.Name : null));
+            .ForMember(dest => dest.ManufacturerName, opt => opt.MapFrom(src => src.Manufacturer.Name))
+            .ForMember(dest => dest.ModelName, opt => opt.MapFrom(src => src.Model.Name))
+            .ForMember(dest => dest.UnitName, opt => opt.MapFrom(src => src.Unit.Name))
+            .ForMember(dest => dest.BuildingName, opt => opt.MapFrom(src => src.Building.Name))
+            .ForMember(dest => dest.Floor, opt => opt.MapFrom(src => src.Building.Floor.ToString()))
+            .ForMember(dest => dest.DepartmentName, opt => opt.MapFrom(src => src.Department.Name));
     }
 }
--- backendInventory.Application/Services/Printers/ViewModel/PrinterViewModel.cs
+++ backendInventory.Application/Services/Printers/ViewModel/PrinterViewModel.cs
@@ -8,6 +8,7 @@ public class PrinterViewModel
     public string? ModelName { get; set; }
     public string? UnitName { get; set; }
     public string? BuildingName { get; set; }
+    public string? Floor { get; set; }
     public string? DepartmentName { get; set; }
     public string? IPAddress { get; set; }
     public string? PrintQueue { get; set; }    
